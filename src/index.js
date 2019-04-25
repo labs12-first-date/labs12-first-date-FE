5e22c6d76ff20a40c49fff3750d77361fb6cb6a4
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-25 14:16:59 -0400

    Remove service worker boilerplate

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { AuthProvider } from './contexts/AuthContext';
 import 'normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.render(
   <AuthProvider>
@@ -14,8 +13,3 @@ ReactDOM.render(
   </AuthProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
