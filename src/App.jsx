7bbd9aaf1ac514e7bee3f9e55880e5ecf13f0b4e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-25 00:16:12 -0400

    Add Navigation and Logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import Login from './components/Login';
+import Logout from './components/Logout';
+import { UserProvider } from './contexts/UserContext';
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/login" component={Login} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </UserProvider>
   );
 };
 
