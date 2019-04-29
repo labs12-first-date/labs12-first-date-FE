672585fa26a14173505bf76a08b35e87f74d493f
Gill Abada <gill@callifi.com>
2019-04-29 15:24:46 -0400

    add email and password inputs to signup form.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
+import useForm from '../hooks/useForm';
 
 const Signup = () => {
   return (
     <>
       <h1>Sign up now to find the love of your life!</h1>
+      <form>
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+      </form>
     </>
   );
 };
+
+export default Signup;
