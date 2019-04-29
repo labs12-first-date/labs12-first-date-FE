637a7ab10a594874eec47b341060d461c491e102
Gill Abada <gill@callifi.com>
2019-04-29 15:16:08 -0400

    create signup component.

---
+++ src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+
+const Signup = () => {
+  return (
+    <>
+      <h1>Sign up now to find the love of your life!</h1>
+    </>
+  );
+};
