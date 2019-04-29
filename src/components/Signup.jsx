e6950d95b38adba4bbc9284516508908f5bf1309
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-29 15:33:49 -0400

    Add sign up form guts

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,12 +4,26 @@ import firebase from 'firebase';
 import useForm from '../hooks/useForm';
 
 const Signup = () => {
+  const { values, handleChange, handleSubmit } = useForm(() => {
+    // on submit
+  });
   return (
     <>
       <h1>Sign up now to find the love of your life!</h1>
-      <form>
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='Password' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={values.email || ''}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={values.password || ''}
+          onChange={handleChange}
+        />
+        <button>Sign up</button>
       </form>
     </>
   );
