a8b771ac648d7f7fb309e1d144f5db8952e7dfaa
Gill Abada <gill@callifi.com>
2019-05-01 18:59:35 -0400

    add reset password form to login page.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
+import useForm from '../hooks/useForm';
+import { auth, resetPasswordNoLogin } from '../firebase';
 
 // Configure FirebaseUI.
 const uiConfig = {
@@ -17,9 +19,22 @@ const uiConfig = {
 };
 
 const Login = () => {
+  const { values, handleChange, handleSubmit } = useForm(() => {
+    auth.resetPasswordNoLogin(values.email);
+  });
   return (
     <div>
       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      <form onSubmit={handleSubmit}>
+        <input
+          name='email'
+          type='email'
+          placeholder='Email'
+          value={values.email || ''}
+          onChange={handleChange}
+        />
+        <button>Reset Password</button>
+      </form>
     </div>
   );
 };
