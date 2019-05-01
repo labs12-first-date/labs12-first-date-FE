f24276648740eedcc8620ba1e194b14dfdda0e68
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-04-30 23:28:31 -0400

    Resolve merge conflicts

--- src/components/onboarding/Card.jsx
+++ src/components/onboarding/Card.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { animated, interpolate } from 'react-spring';
+// import useForm from '../../hooks/useForm';
+// import { MenuItem } from '@blueprintjs/core';
 import { DateInput } from '@blueprintjs/datetime';
 // import Carousel from 'nuka-carousel';
 import Select from 'react-select';
@@ -11,86 +13,94 @@ const jsDateFormatter = {
   placeholder: 'M/D/YYYY'
 };
 
-const renderInput = (p, handleChange, formVals) => {
-  switch (p.inputType) {
-    case 'text':
-      return (
-        <input
-          type='text'
-          placeholder={p.inputPlaceholder}
-          name={p.fieldName}
-        />
-      );
-    case 'textarea':
-      return (
-        <input
-          type='textarea'
-          placeholder={p.inputPlaceholder}
-          name={p.fieldName}
-        />
-      );
-    case 'number':
-      return (
-        <input
-          type='number'
-          placeholder={p.inputPlaceholder}
-          name={p.fieldName}
-        />
-      );
-    case 'multiSelect':
-      return (
-        <Select
-          value={formVals.value}
-          name={p.fieldName}
-          onChange={handleChange}
-          options={p.choices}
-          isMulti
-        />
-      );
-    case 'dateInput':
-      return (
-        <DateInput
-          formatDate={date => date.toLocaleString()}
-          //  onChange={this.handleDateChange}
-          parseDate={str => new Date(str)}
-          placeholder={'M/D/YYYY'}
-          {...jsDateFormatter}
-          //  value={this.state.date}
-        />
-      );
-    default:
-      return <div>No renderer for "{p.fieldName}"</div>;
-  }
-};
-
 const Card = props => {
-  const [formVals, setFormVals] = useState({});
+  const [formValues, setFormValues] = useState({});
 
-  const handleChange = selectedOption => {
-    setFormVals(previousVals => {
-      return { ...previousVals, selectedOption };
+  // just for logging / sanity
+  useEffect(() => {
+    console.log('FORM STATE CHANGE:', formValues);
+  }, [formValues]);
+
+  // const handleChangeForMultiSelect = ({ field, value }) => {
+  //   setFormValues(previousValues => {
+  //     // const current = previousValues.field || [];
+  //     return { ...previousValues, [field]: value };
+  //   });
+  // };
+
+  const handleChange = ({ field, value }) => {
+    setFormValues(previousValues => {
+      return { ...previousValues, [field]: value };
     });
   };
 
   const { i, x, y, rot, scale, trans, bind, data } = props;
   const { cardTitle, onboardingStep, prompts } = data[i];
 
+  const renderInput = p => {
+    switch (p.inputType) {
+      case 'text':
+        return (
+          <input
+            type="text"
+            placeholder={p.inputPlaceholder}
+            name={p.fieldName}
+            value={formValues[p.fieldName] || ''}
+            onChange={e => handleChange({ field: p.fieldName, value: e.target.value })}
+          />
+        );
+      case 'number':
+        return (
+          <input
+            type="number"
+            placeholder={p.inputPlaceholder}
+            name={p.fieldName}
+            value={formValues[p.fieldName] || ''}
+            onChange={e => handleChange({ field: p.fieldName, value: e.target.value })}
+          />
+        );
+      case 'multiSelect':
+        return (
+          <Select
+            value={formValues[p.fieldName] || []}
+            name={p.fieldName}
+            onChange={value => handleChange({ field: p.fieldName, value: value })}
+            options={p.choices}
+            isMulti
+          />
+        );
+      case 'dateInput':
+        return (
+          <DateInput
+            formatDate={date => date.toLocaleString()}
+            onChange={value => handleChange({ field: p.fieldName, value })}
+            parseDate={str => new Date(str)}
+            placeholder={'M/D/YYYY'}
+            {...jsDateFormatter}
+            //  value={this.state.date}
+          />
+        );
+      default:
+        return <div>No renderer for "{p.fieldName}"</div>;
+    }
+  };
+
   return (
     <animated.div
-      className='ani1'
+      className="ani1"
       key={i}
       style={{
         transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`)
       }}
     >
       <animated.div
-        className='ani2'
+        className="ani2"
         {...bind(i)}
         style={{
           transform: interpolate([rot, scale], trans)
         }}
       >
-        <div className='card'>
+        <div className="card">
           {/* <Carousel>
               {pics.map((pic, index) => (
                 <img src={pic} key={index} alt='profilePicture' />
@@ -100,11 +110,10 @@ const Card = props => {
           <form>
             <h2>{cardTitle}</h2>
             {prompts.map(p => (
-              <>
+              <div key={p.fieldName}>
                 <h3>{p.prompt}</h3>
-
-                {renderInput(p, handleChange, formVals)}
-              </>
+                {renderInput(p)}
+              </div>
             ))}
 
             <p>
