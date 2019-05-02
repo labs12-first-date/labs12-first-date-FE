688b38abaa14b26536e22522ba2ac5a4258a76c1
Gill Abada <gill@callifi.com>
2019-05-02 10:33:42 -0400

    add checks to redirect users if onboarding is not complete.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Logout from './components/Logout';
 import Signup from './components/Signup';
 import Settings from './components/Settings';
 // import Dashboard from './components/Dashboard';
-import Swipe from './components/onboarding/Deck';
+import Onboarding from './components/onboarding/Deck';
+import Thunderdome from './components/thunderDome/Deck';
 
 import Profile from './components/Profile';
 import { AuthContext } from './contexts/AuthContext';
@@ -22,20 +23,21 @@ const App = () => {
       <>
         {/* <div className='container'> */}
         <Navigation />
-        <h1 className="center">The Most Awkward Dating</h1>
+        <h1 className='center'>The Most Awkward Dating</h1>
         {user && (
-          <p className="center">
+          <p className='center'>
             Signed in as <em>{user.email}</em>
           </p>
         )}
         <Switch>
           {/* <Route exact path="/" component={Dashboard} /> */}
-          <Route path="/login" component={Login} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/swipe" component={Swipe} />
-          <Route path="/settings" component={Settings} />
+          <Route path='/login' component={Login} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/welcome' component={Onboarding} />
+          <Route path='/settings' component={Settings} />
+          <Route path='/thunderdome' component={Thunderdome} />
         </Switch>
         {/* </div> */}
       </>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -56,38 +56,38 @@ const Profile = () => {
             <Loading />
           ) : (
             <Card elevation={Elevation.TWO}>
-              <div className="container">
-                <div className="card grey lighten-1  ">
-                  <div class="card-content black-text">
-                    <span class="card-title">Profile</span>
-                    <ul className="row">
-                      <li className="col s12">
-                        <span className="red-text text-darken-2">
+              <div className='container'>
+                <div className='card grey lighten-1  '>
+                  <div class='card-content black-text'>
+                    <span class='card-title'>Profile</span>
+                    <ul className='row'>
+                      <li className='col s12'>
+                        <span className='red-text text-darken-2'>
                           First Name
                         </span>{' '}
                         {data.first_name}
                       </li>
-                      <li className="col s12">
-                        <span className="red-text text-darken-2">
+                      <li className='col s12'>
+                        <span className='red-text text-darken-2'>
                           Last Name
                         </span>{' '}
                         {data.last_name}{' '}
                       </li>
 
-                      <li className="col s12">DOB {data.DOB}</li>
-                      <li className="col s12">Bio {data.bio}</li>
-                      <li className="col s12">
+                      <li className='col s12'>DOB {data.DOB}</li>
+                      <li className='col s12'>Bio {data.bio}</li>
+                      <li className='col s12'>
                         Condition details {data.condition_details}
                       </li>
 
-                      <li className="col s12">Likes {data.likes}</li>
-                      <li className="col s12">
+                      <li className='col s12'>Likes {data.likes}</li>
+                      <li className='col s12'>
                         Looking for {data.looking_for}
                       </li>
-                      <li className="col s12">
+                      <li className='col s12'>
                         Your contidion {data.what_ails_you}
                       </li>
-                      <li className="col s12">Zip Code {data.zip_code}</li>
+                      <li className='col s12'>Zip Code {data.zip_code}</li>
                     </ul>
                     <Button onClick={showForm}>Update</Button>
                   </div>
@@ -95,34 +95,34 @@ const Profile = () => {
 
                 <Overlay usePortal={true} isOpen={toggleState}>
                   <Card elevation={Elevation.TWO}>
-                    <form id="profileForm" onSubmit={handleSubmit}>
+                    <form id='profileForm' onSubmit={handleSubmit}>
                       <input
-                        name="first_name"
-                        placeholder="First Name"
+                        name='first_name'
+                        placeholder='First Name'
                         value={values.first_name || ` ${formState.first_name}`}
                         onChange={handleChange}
                       />
                       <input
-                        name="last_name"
-                        placeholder="Last Name"
+                        name='last_name'
+                        placeholder='Last Name'
                         value={values.last_name || ` ${formState.last_name}`}
                         onChange={handleChange}
                       />
                       <input
-                        name="DOB"
-                        placeholder="DOB"
+                        name='DOB'
+                        placeholder='DOB'
                         value={values.DOB || ` ${formState.DOB}`}
                         onChange={handleChange}
                       />
                       <input
-                        name="bio"
-                        placeholder="Bio"
+                        name='bio'
+                        placeholder='Bio'
                         value={values.bio || ` ${formState.bio}`}
                         onChange={handleChange}
                       />
                       <input
-                        name="condition_details"
-                        placeholder="Condition Details"
+                        name='condition_details'
+                        placeholder='Condition Details'
                         value={
                           values.condition_details ||
                           ` ${formState.condition_details}`
@@ -130,34 +130,34 @@ const Profile = () => {
                         onChange={handleChange}
                       />
                       <input
-                        name="likes"
-                        placeholder="Likes"
+                        name='likes'
+                        placeholder='Likes'
                         value={values.likes || ` ${formState.likes}`}
                         onChange={handleChange}
                       />
                       <input
-                        name="looking_for"
-                        placeholder="Looking For"
+                        name='looking_for'
+                        placeholder='Looking For'
                         value={
                           values.looking_for || ` ${formState.looking_for}`
                         }
                         onChange={handleChange}
                       />
                       <input
-                        name="what_ails_you"
-                        placeholder="Your Condition"
+                        name='what_ails_you'
+                        placeholder='Your Condition'
                         value={
                           values.what_ails_you || ` ${formState.what_ails_you}`
                         }
                         onChange={handleChange}
                       />
                       <input
-                        name="zip_code"
-                        placeholder="Zip Code"
+                        name='zip_code'
+                        placeholder='Zip Code'
                         value={values.zip_code || ` ${formState.zip_code}`}
                         onChange={handleChange}
                       />
-                      <Button onClick={showForm} type="submit">
+                      <Button onClick={showForm} type='submit'>
                         PressMe
                       </Button>
                     </form>
