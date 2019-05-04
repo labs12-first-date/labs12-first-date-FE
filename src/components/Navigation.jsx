f9890414874a48cd749efdbb06fc98d5e39bacbb
UnknownMonk <smodcartoon@gmail.com>
2019-05-04 09:26:07 -0400

    fixed on boarding and nav issues so it checks if your profile is complete before it let you acess profile

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,44 +1,78 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { FirestoreDocument } from 'react-firestore';
+import Loading from './Loading';
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import './Navigation.css';
 const Navigation = () => {
+  const [formState, setformState] = useState({});
   const { user } = useContext(AuthContext);
+
+  console.log('more form state', formState);
 
   return (
     <nav>
-      <ul>
-        <>
+      {user ? (
+        <FirestoreDocument
+          path={`profiles/${user.uid}`}
+          render={({ isLoading, data }) => {
+            if (isLoading) {
+              console.log('This is the nav uid', user.uid);
+
+              return <Loading />;
+            } else if (!data.profile_completed) {
+              return (
+                <ul>
+                  <li>
+                    <NavLink to="/">Home</NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/welcome">Onboard</NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/logout">Log out</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/profile">Profile</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/settings">Settings</NavLink>
+                  </li>
+                </ul>
+              );
+            } else {
+              return (
+                <ul>
+                  <li>
+                    <NavLink to="/">Home</NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/logout">Log out</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/profile">Profile</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/settings">Settings</NavLink>
+                  </li>
+                </ul>
+              );
+            }
+          }}
+        />
+      ) : (
+        <ul>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/login">Log in</NavLink>
           </li>
           <li>
-            <NavLink to="/welcome">Onboard</NavLink>
+            <NavLink to="/signup">Sign Up</NavLink>
           </li>
-        </>
-        {user ? (
-          <>
-            <li>
-              <NavLink to="/logout">Log out</NavLink>
-            </li>
-            <li>
-              <NavLink to="/profile">Profile</NavLink>
-            </li>
-            <li>
-              <NavLink to="/settings">Settings</NavLink>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <NavLink to="/login">Log in</NavLink>
-            </li>
-            <li>
-              <NavLink to="/signup">Sign Up</NavLink>
-            </li>
-          </>
-        )}
-      </ul>
+        </ul>
+      )}
     </nav>
   );
 };
