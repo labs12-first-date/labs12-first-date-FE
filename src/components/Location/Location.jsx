19106b967503c8fd8d0a9d647dcc2488c2d8ad28
VaderSteve76 <alverson.s@yahoo.com>
2019-05-07 17:58:13 -0400

    User Profile Data

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,33 +1,73 @@
+import { firebase, auth } from '../../firebase';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Loading from '../Loading';
 
 const LocationDistance = () => {
-  const [location, setLocation] = useState(null);
+  const [user] = useState(auth.getCurrentUser());
+  const [profileState, setProfileState] = useState(null);//profile
+  const [location, setLocation] = useState(null); //location
 
+
+  //Gets user profile
   useEffect(() => {
-    const getData = async () => {
-      const result = await axios(
-        'https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=32927&minimumradius=0&maximumradius=10&key=DEMOAPIKEY'
-      );
-      setLocation(result.data);
-    };
-    getData();
+    const docRef = firebase
+      .firestore()
+      .collection('profiles')
+      .doc(user.uid);
+    docRef
+      .get()
+      .then(function(doc) {
+        setProfileState(doc.data());
+        //console.log('DOC', doc.data());
+      })
+      .catch(function(error) {
+        console.log('Error getting document:', error);
+        // history.replace('/welcome');
+      });
+      console.log('PROFILE useEffect')
+
+
   }, []);
 
-  return !location ? (
+
+  //Get location radius array
+  useEffect(() => {
+    if(profileState) {
+      const zip = profileState.zip_code;
+      const getData = async () => {
+        const result = await axios(
+          `https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=${zip}&minimumradius=0&maximumradius=10&key=DEMOAPIKEY`
+        );
+        console.log('LOCATION useEffect');
+        setLocation(result.data);
+  
+      };
+      getData();
+    }
+  }, [profileState]);
+
+
+
+  console.log('PROFILE STATE is', profileState);
+  console.log('LOCATION is', location);
+
+
+  return (!location) ? (
     <Loading />
   ) : (
     <div>
       {/* <p>Location does exist!!!</p> */}
       {console.log(
-        'after useEffect',
+        'API data is mapped',
         location.DataList.map(data => {
           return data.Code;
         })
       )}
     </div>
   );
+
+
 };
 
 export default LocationDistance;
