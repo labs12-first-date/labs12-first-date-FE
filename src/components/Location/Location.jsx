01f66a82d93cb023aab2c0a6dd4653c24d32f73b
VaderSteve76 <alverson.s@yahoo.com>
2019-05-07 07:43:04 -0400

    Retruning Mapped Data

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -3,25 +3,33 @@ import axios from 'axios';
 
 
 const LocationDistance = () => {
-  const [location, setLocation] = useState([
-
-  ]);
+  const [location, setLocation] = useState(null);
 
   useEffect(async () => {
     const result = await axios(
       'https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=32927&minimumradius=0&maximumradius=10&key=DEMOAPIKEY',
     );
-
     setLocation(result.data);
   }, []);
-console.log(location);
-  
-    return (
-      <div>
-        
-      </div>
-    )
-  }
 
+  
+  {if (!location) {
+    return (
+    <div>
+        {/* <p>There is no location!!!</p> */}
+        {console.log('before useEffect', location)}
+    </div>
+    )
+  } else {
+    return (
+        <div>
+            {/* <p>Location does exist!!!</p> */}
+            {console.log('after useEffect', location.DataList.map(data => {
+                return data.Code;
+            }))}
+        </div>
+        )
+  }}
+}
 
 export default LocationDistance;  
