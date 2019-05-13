943d39b144a5a2c0b83c93fa3fbfdcca0bb6f445
James Basile <jbasile145@gmail.com>
2019-05-13 16:41:50 -0400

    added form child to stripeApp componenet

--- src/components/stripe/StripeApp.jsx
+++ src/components/stripe/StripeApp.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StripeProvider, Elements } from 'react-stripe-elements';
 import Form from './Form';
 
-export default class App extends React.Component<IAppProps, IAppState> {
+export default class StripeApp extends React.Component<IAppProps, IAppState> {
 
 
 
@@ -11,7 +11,7 @@ export default class App extends React.Component<IAppProps, IAppState> {
   render() {
     return (
       <>
-      <StripeProvider api="pk_test_ojgxSZHYR90grl2IAcjSjgoc00Rn3bEDfA">
+      <StripeProvider apiKey="pk_test_ojgxSZHYR90grl2IAcjSjgoc00Rn3bEDfA">
       <Elements>
         <Form />
       </Elements>
