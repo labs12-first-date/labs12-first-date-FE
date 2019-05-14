fd41bb2d14ac07a3e9317b4aa295b2bbba05d292
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-14 16:36:46 -0400

    Major refactor for clarity

--- src/components/thunderDome/ThunderDeck.jsx
+++ src/components/thunderDome/ThunderDeck.jsx
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useSprings } from 'react-spring';
 import { useGesture } from 'react-with-gesture';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { auth, firebase } from '../../firebase';
 import Loading from '../Loading';
 import './ThunderDome.css';
 import MatchCard from './MatchCard';
-import convertDescriptorToString from 'jest-util/build/convertDescriptorToString';
-import LocationDistance from '../Location/Location';
 
+import runMatchAlgo from '../../helpers/matching';
 import recordSwipe from '../../helpers/swipeActions';
 import Navigation from '../Navigation';
 
+const db = firebase.firestore();
+
+const NoMatches = styled.div`
+  color: #eee;
+  font-size: 2em;
+  text-align: center;
+  margin-top: 4rem;
+`;
 
 const to = i => ({
   x: 10,
@@ -25,175 +34,83 @@ const trans = (r, s) =>
   `perspective(1500px) rotateX(20deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`;
 
 const ThunderDeck = ({ history }) => {
-  const [user] = useState(auth.getCurrentUser());
-  const [profileState, setprofileState] = useState(null);
-  const [profileData, setProfileData] = useState([]);
-  const [settingsState, setsettingsState] = useState(null);
+  const user = auth.getCurrentUser();
+  const [userProfile, setUserProfile] = useState(null);
+  const [userSettings, setUserSettings] = useState(null);
+  const [potentialMatches, setPotentialMatches] = useState([]);
+  const [noMatches, setNoMatches] = useState(false);
+
+  // 1. on mount, get user profile, check if complete
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const snapshot = await db
+          .collection('profiles')
+          .doc(user.uid)
+          .get();
+        const profile = snapshot.data();
+        // is user's profile complete?
+        if (userProfile && !userProfile.profile_completed) {
+          history.replace('/welcome');
+        } else {
+          setUserProfile(profile);
+        }
+      } catch (error) {
+        console.error('Error getting user profile: ', error);
+      }
+    };
+    fetchUserProfile();
+  }, []);
+
+  // 2. after we have a user, get user settings
+  useEffect(() => {
+    const fetchUserSettings = async () => {
+      try {
+        const settingsSnapshot = await db
+          .collection('settings')
+          .doc(user.uid)
+          .get();
+        const settings = settingsSnapshot.data();
+        setUserSettings(settings);
+      } catch (error) {
+        console.error('Error getting user settings: ', error);
+      }
+    };
+
+    if (user) fetchUserSettings();
+  }, [user]);
+
+  // 3. after we have user settings, fetch potential matches
+  useEffect(() => {
+    const fetchProfilesForMatching = async () => {
+      setNoMatches(false);
+      const min_age = userSettings.match_age_min || 18;
+      const max_age = userSettings.match_age_max || 99;
+      const profilesSnapshot = await db
+        .collection('profiles')
+        .where('age', '>=', min_age)
+        .where('age', '<=', max_age)
+        .limit(50)
+        .get();
+      const profiles = profilesSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      const potentialMatches = runMatchAlgo(userProfile, profiles);
+      if (!potentialMatches.length) {
+        setNoMatches(true);
+      } else {
+        setPotentialMatches(potentialMatches);
+      }
+    };
+
+    if (userSettings) fetchProfilesForMatching();
+  }, [userSettings, userProfile]);
 
   const swipe = (swipedUserId, isLike) => {
     recordSwipe(user.uid, swipedUserId, isLike);
   };
 
-  useEffect(() => {
-    if (user) {
-      const docRef = firebase
-        .firestore()
-        .collection('settings')
-        .doc(user.uid);
-      docRef
-        .get()
-        .then(function(doc) {
-          setsettingsState(doc.data());
-        })
-        .catch(function(error) {
-          console.log('Error getting document:', error);
-        });
-    }
-  }, [user]);
-
-  console.log('UID', user.uid);
-
-  const wantedTraits = profileState => {
-    let wantedConds = [];
-    const conditon = profileState.match_conditions.map(matchC => {
-      return matchC;
-    });
-    const moreCondition = conditon.map(c => {
-      return c.value;
-    });
-    wantedConds.push(moreCondition);
-    return wantedConds;
-  };
-
-  const wantedGenders = profileState => {
-    let wantedGens = [];
-    console.log('Match Gender in wANTED gENDER', profileState.match_gender);
-    const gender = profileState.match_gender.map(matchC => {
-      return matchC;
-    });
-    const moreGender = gender.map(c => {
-      return c.value;
-    });
-    wantedGens.push(moreGender);
-    return wantedGens;
-  };
-
-  const matchAlgo = potMatch => {
-    const zipCodes = profileState.nearby_zip;
-    const tempConditions = wantedTraits(profileState)[0];
-    const compGender = wantedGenders(profileState)[0];
-    // const matches = potMatch.filter(match => zipCodes.includes(match.zip_code)); //filter by zipcode;
-    const matches = potMatch;
-    let foundMatches = [];
-    for (let match of matches) {
-      for (let condition of match.conditions) {
-        for (let matched_cond of tempConditions) {
-          if (matched_cond === condition.value) {
-            foundMatches.push(match);
-          }
-        }
-      }
-    }
-
-    let foundGender = [];
-    for (let match of foundMatches) {
-      for (let gender of match.gender) {
-        for (let matched_gen of compGender) {
-          if (matched_gen === gender.value) {
-            console.log(match.profile_uid);
-            foundGender.push(match);
-          }
-        }
-      }
-    }
-
-    setProfileData(foundGender);
-
-    // const michael = matches.filter(a => {
-    //   console.log('MICHAEL RUNNING');
-    //   return (
-    //     a.conditions.find(c => {
-    //       return (
-    //         c.value ===
-    //         TempConditions.find(x => {
-    //           return x === c.value;
-    //         })
-    //       );
-    //     }) !== undefined
-    //   );
-    // });
-    // console.log('MICHAEL', michael);
-
-    // const mm = matches.map(p => {
-    //   console.log('CONDITIONS', p.conditions);
-    //   const x = p.conditions;
-    //   x.map(y => {
-    //     console.log(y.value);
-    //     if (TempConditions.includes(y.value)) {
-    //       return p;
-    //     }
-    //   });
-  };
-
-  useEffect(() => {
-    if (settingsState) {
-      const max_age = settingsState.match_age_max;
-      const min_age = settingsState.match_age_min;
-
-      if (max_age && min_age) {
-        const profiles = firebase
-          .firestore()
-          .collection('profiles')
-          .where('age', '>=', min_age)
-          .where('age', '<=', max_age)
-          .limit(500)
-          .get()
-          .then(function(querySnapShot) {
-            const potMatches = querySnapShot.docs.map(function(doc) {
-              return { ...doc.data(), id: doc.id };
-            });
-            matchAlgo(potMatches);
-          });
-      }
-    }
-  }, [settingsState]);
-
-  console.log('ProfileData here', profileData);
-
-  useEffect(() => {
-    const docRef = firebase
-      .firestore()
-      .collection('profiles')
-      .doc(user.uid);
-    docRef
-      .get()
-      .then(function(doc) {
-        setprofileState(doc.data());
-        console.log('DOC', doc.data());
-      })
-      .catch(function(error) {
-        console.log('Error getting document:', error);
-        history.replace('/welcome');
-      });
-  }, []);
-
-  // useEffect(() => {
-  //   if (profileState && profileState.match_conditions) {
-  //     wantedTraits(profileState);
-  //   }
-  // }, [profileState]);
-
-  useEffect(() => {
-    if (profileState && !profileState.profile_completed) {
-      console.log(profileState);
-      console.log('thunder!!!', profileState.profile_completed);
-      history.replace('/welcome');
-    }
-  }, [profileState]);
-
   const [gone] = useState(() => new Set());
 
-  const [props, set] = useSprings(profileData.length, i => ({
+  const [props, set] = useSprings(potentialMatches.length, i => ({
     ...to(i),
     from: from(i)
   }));
@@ -216,7 +133,7 @@ const ThunderDeck = ({ history }) => {
 
         const scale = down ? 1.1 : 1;
         // if (dir === 1) {
-        //   console.log('WHAT?', profileData);
+        //   console.log('WHAT?', potentialMatches);
         //   // console.log('Direction: right index:', index);
         // } else {
         //   // console.log('Direction: left index:', index);
@@ -236,29 +153,44 @@ const ThunderDeck = ({ history }) => {
         };
       });
 
-      if (!down && gone.size === profileData.length)
+      if (!down && gone.size === potentialMatches.length)
         console.log('Cards are done. Let the DB know this person is ready to date!');
     }
   );
 
-  if (profileData) {
-    return props.map(({ x, y, rot, scale }, i) => (
+  if (noMatches)
+    return (
+      <div>
+        <Navigation />
+        <NoMatches>
+          Sorry, no matches :/ <br /> <Link to="/settings">Update settings</Link>
+        </NoMatches>
+      </div>
+    );
+
+  if (potentialMatches.length) {
+    return (
       <>
         <Navigation />
-        <MatchCard
-          className="card"
-          i={i}
-          x={x}
-          y={y}
-          rot={rot}
-          scale={scale}
-          trans={trans}
-          data={profileData[i]}
-          bind={bind}
-        />
-        <LocationDistance />
+        {props.map(({ x, y, rot, scale }, i) => {
+          const match = potentialMatches[i];
+          return (
+            <MatchCard
+              key={match.id}
+              className="card"
+              i={i}
+              x={x}
+              y={y}
+              rot={rot}
+              scale={scale}
+              trans={trans}
+              data={match}
+              bind={bind}
+            />
+          );
+        })}
       </>
-    ));
+    );
   } else {
     return <Loading />;
   }
