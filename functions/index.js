6f9414a83a21a0e3e258e2153646a7f0413e16c5
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-14 19:51:57 -0400

    Add getZip function

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors({ origin: true }));
+
+const API_KEY = 'ElfcJlFIXxr0ySR9hkgoiqDjnNdw8yhTbB7qGU0wMvvWprpzVyfZbIn83AE7wDEr';
+
+app.get('/', async (req, res) => {
+  try {
+    const { zip, distance } = req.query;
+    console.log(zip, distance);
+    const endPoint = `https://www.zipcodeapi.com/rest/${API_KEY}/radius.json/${zip}/${distance}/mile`;
+    const { data } = await axios.get(endPoint);
+    zips = data.zip_codes.map(z => z.zip_code);
+    return res.status(200).json(zips);
+  } catch (error) {
+    return res.status(400).json({ error: `${error}` });
+  }
+});
+
+exports.getZip = functions.https.onRequest(app);
