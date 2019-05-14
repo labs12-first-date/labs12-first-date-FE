86e8647e56d80ea8b5ac32041df19c43d653df1e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-05-14 16:46:35 -0400

    Tidy up

--- src/components/thunderDome/MatchCard.jsx
+++ src/components/thunderDome/MatchCard.jsx
@@ -1,97 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './ThunderDome.css';
 import PropTypes from 'prop-types';
 import { animated, interpolate } from 'react-spring';
-import { auth } from '../../firebase';
-import Loading from '../Loading';
-// import useForm from '../../hooks/useForm';
-// import { MenuItem } from '@blueprintjs/core';
-
-// const Card = () => {
-//   return (
-//     <div className='thunder-heading'>
-//       <h1>Welcome to the Thunderdome!</h1>
-//       <h2>Coming May 2019</h2>
-//     </div>
-//   );
-// };
 
 const MatchCard = props => {
-  const [formValues, setFormValues] = useState({});
-  const [user] = useState(auth.getCurrentUser());
-  console.log('Props on matchcard', props);
-  // console.log('this is first name', props.data.first_name);
-  // console.log('this is profile pic near FN', props.data.profile_picture);
-
-  // just for logging / sanity
-  // useEffect(() => {
-  //   console.log('FORM STATE CHANGE:', formValues);
-  //   firebase
-  //     .firestore()
-  //     .collection('profiles')
-  //     .doc(user.uid)
-  //     .update(formValues)
-  //     .then(function() {
-  //       console.log('Document successfully written!');
-  //     });
-  // }, [formValues]);
-
-  const handleChange = ({ field, value }) => {
-    setFormValues(previousValues => {
-      return { ...previousValues, [field]: value };
-    });
-  };
-
   const { i, x, y, rot, scale, trans, bind, data } = props;
-  console.log('More props', props);
+  const { first_name, age, zip_code, bio, gender, profile_picture, id: uid } = data;
 
-  const {
-    first_name,
-    age,
-    zip_code,
-    bio,
-    gender,
-    profile_picture,
-    id: uid
-  } = data;
-
-  if (data === []) {
-    return <Loading />;
-  } else {
-    return (
+  return (
+    <animated.div
+      className="ani1"
+      key={i}
+      style={{
+        transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`)
+      }}
+    >
       <animated.div
-        className='ani1'
-        key={i}
+        className="ani2"
+        {...bind(i, uid)}
         style={{
-          transform: interpolate(
-            [x, y],
-            (x, y) => `translate3d(${x}px,${y}px,0)`
-          )
+          transform: interpolate([rot, scale], trans)
         }}
       >
-        <animated.div
-          className='ani2'
-          {...bind(i, uid)}
-          style={{
-            transform: interpolate([rot, scale], trans)
-          }}
-        >
-          <div className='card'>
-            <img src={profile_picture} alt='profilePicture' />
-            <h2>{first_name},</h2>
-            <h2>{age},</h2>
-            <h5>{zip_code}</h5>
-            <h5>{bio}</h5>,
-            <h5>
-              {gender.map(g => {
-                return g.value + '  ';
-              })}
-            </h5>
-          </div>
-        </animated.div>
+        <div className="card">
+          <img src={profile_picture} alt="profilePicture" />
+          <h2>{first_name},</h2>
+          <h2>{age},</h2>
+          <h5>{zip_code}</h5>
+          <h5>{bio}</h5>,
+          <h5>
+            {gender.map(g => {
+              return g.value + '  ';
+            })}
+          </h5>
+        </div>
       </animated.div>
-    );
-  }
+    </animated.div>
+  );
 };
 // };
 
